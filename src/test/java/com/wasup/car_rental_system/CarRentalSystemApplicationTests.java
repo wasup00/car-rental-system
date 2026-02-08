9938d1cf2901dffe53501975811465a91aac6c48
Guillaume DHOTEL <guillaume.dhotel1@gmail.com>
2026-02-08 17:48:07 +0100

    Initial commit

---
+++ src/test/java/com/wasup/car_rental_system/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasup.car_rental_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
